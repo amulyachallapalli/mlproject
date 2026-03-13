98dc9532b5dff60a040553bafbcbd9a87df89a72
amulyachallapalli <amulyachallapalli@gmail.com>
2026-03-13 18:29:04 +0000

    Second commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOR="-e ."
+def get_requirements(file_path:str)->List[str]:
+    
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements= [req.replace("\n","") for req in requirements]
+        if HYPEN_E_DOR in requirements:
+            requirements.remove(HYPEN_E_DOR)
+    
+    return requirements
+    
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Amulya',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
